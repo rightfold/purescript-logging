1ba7f2c8fd2f218567230f80f5f2fe7c44a7df94
rightfold <rightfold@gmail.com>
2016-08-21 16:22:29 +0200

    Remove Show constraint from console logger
    Instead, the user can provide their own print function. Show is for
    converting values to their source code representation, which is a
    completely different use-case.

--- src/Control/Logger/Console.purs
+++ src/Control/Logger/Console.purs
@@ -5,12 +5,10 @@ module Control.Logger.Console
 import Control.Logger (Logger(Logger))
 import Control.Monad.Eff.Class (liftEff, class MonadEff)
 import Control.Monad.Eff.Console (CONSOLE, log)
-import Prelude
 
 -- | Logger that logs records to the console.
 console :: forall m e r
-         . ( MonadEff (console :: CONSOLE | e) m
-           , Show r
-           )
-        => Logger m r
-console = Logger \r -> liftEff (log (show r))
+         . (MonadEff (console :: CONSOLE | e) m)
+        => (r -> String)
+        -> Logger m r
+console show = Logger \r -> liftEff (log (show r))
