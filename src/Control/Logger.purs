2464b9b5cae7e21925e153c2435dabd7ff15aa0f
rightfold <rightfold@gmail.com>
2016-08-27 18:00:11 +0200

    Add Divisible instance

--- src/Control/Logger.purs
+++ src/Control/Logger.purs
@@ -5,6 +5,7 @@ module Control.Logger
 ) where
 
 import Data.Divide (class Divide)
+import Data.Divisible (class Divisible)
 import Data.Functor.Contravariant (class Contravariant)
 import Data.Monoid (class Monoid)
 import Data.Tuple (Tuple(..))
@@ -19,6 +20,9 @@ instance contravariantLogger :: Contravariant (Logger m) where
 instance divideLogger :: (Apply m) => Divide (Logger m) where
   divide f (Logger a) (Logger b) =
     Logger \r -> case f r of Tuple r1 r2 -> a r1 *> b r2
+
+instance divisibleLogger :: (Applicative m) => Divisible (Logger m) where
+  conquer = Logger \_ -> pure unit
 
 instance semigroupLogger :: (Apply m) => Semigroup (Logger m r) where
   append (Logger a) (Logger b) = Logger \r -> a r *> b r
