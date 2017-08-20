67bb503cf9c6472bd868d1c7ba82c7ab6e8f9afa
rightfold <rightfold@gmail.com>
2017-08-20 15:01:06 +0200

    Update for PureScript 0.12

--- src/Control/Logger/Writer.purs
+++ src/Control/Logger/Writer.purs
@@ -8,8 +8,7 @@ import Control.Monad.Writer.Class (class MonadWriter, tell)
 
 -- | Logger that writes records to the writer.
 writer :: forall m r
-        . ( MonadWriter r m
-          , Monoid r
-          )
+        . MonadWriter r m
+       => Monoid r
        => Logger m r
 writer = Logger tell
